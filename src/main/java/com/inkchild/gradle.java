ab0f845348b5adde5fb9204d30843de18683ae29
Mazhar Ahmed <mazhar266@gmail.com>
2016-07-28 00:45:30 +0600

    achievement unlocked

--- src/main/java/com/inkchild/gradle.java
+++ src/main/java/com/inkchild/gradle.java
@@ -1,9 +1,22 @@
 package com.inkchild;
 
+import org.asynchttpclient.*;
+import java.util.concurrent.Future;
+
 public class gradle {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try {
+            AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient();
+            Future<Response> f = asyncHttpClient.prepareGet("http://www.iammaze.com/").execute();
+            Response r = f.get();
+            System.out.println("Status Code : " + r);
+            
+            asyncHttpClient.close();
+        } catch(Exception e) {
+            System.out.println("Something went wrong.");
+        }
     }
 }
